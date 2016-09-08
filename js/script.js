4283eff76311413f8ed22968b647f27b7c7d1e4b
General Assembly <generalassembly@Generals-MacBook-Air-4.local>
2016-09-08 14:43:13 -0400

    hamburger menu expands when clicked and closes when clicked again. Menu items also jump to the part of the page that it's asscociated with.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+  overlay = $('.overlay'),
+  isClosed = false;
+
+  //call the menu to open
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+
+  //If menu is clicked, change the class to open it
+  //If X(close) is clicked, close the menu
+  function hamburger_cross() {
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+
+  //add.remove the class toggled to the wrapper container when clicked
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });
+});
