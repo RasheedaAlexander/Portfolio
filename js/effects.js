1339195b426ea2460dd95a2dc8be3118a71f61e0
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-10-03 13:16:48 -0400

    Removed fade effect from about section.

--- js/effects.js
+++ js/effects.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
   $(window).scrollTop(0);
   // Show/Hide about div while scrolling
-  $(window).scroll(function() {
+  $(window).resize(function(){
+    if ($(window).width() <= 800){
+      $(window).scroll(function() {
 
-    if ($(this).scrollTop()>0)
-    {
-      $('#about-txt').fadeIn("slow");
-    }
-    else
-    {
-      $('#about-txt').fadeOut("slow");
+        if ($(this).scrollTop()>0)
+        {
+          $('#about-txt').fadeIn("slow");
+        }
+        else
+        {
+          $('#about-txt').fadeOut("slow");
+        }
+      });
     }
   });
 })
